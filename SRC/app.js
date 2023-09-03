1dc1e15284ebbe15565d07947a9fa7566c253ad8
Noni3723 <moyikwan@gmail.com>
2023-09-03 19:55:06 +0200

    Updated html

--- SRC/app.js
+++ SRC/app.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temeprature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let speedElement = document.querySelector("#speed");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  temperatureElement.innerHTMLn = Math.round(response.data.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "b590d66dc52ca21d7ebdfd47412549bf";
+let apiUrl =
+  "api.openweathermap.org/data/2.5/weather?q=New York&APPID=b590d66dc52ca21d7ebdfd47412549bf&units=metric";
 
+axios.get(apiUrl).then(displayTemperature);
