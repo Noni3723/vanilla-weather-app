a8226fab81c5e41da8e857115ecb2ef71ce42d31
Noni3723 <moyikwan@gmail.com>
2023-09-09 22:04:36 +0200

    updated js

--- SRC/app.js
+++ SRC/app.js
@@ -1,19 +1,14 @@
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#temeprature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let speedElement = document.querySelector("#speed");
-  let windElement = document.querySelector("#wind");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
+function search(city) {
+  let apiKey = "b590d66dc52ca21d7ebdfd47412549bf";
+  let apiUrl =
+    "api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric";
+  axios.get(apiUrl).then(displayTemperature);
 }
-let apiKey = "b590d66dc52ca21d7ebdfd47412549bf";
-let apiUrl =
-  "api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric";
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+let form = document.querySelector("search-form");
+form.addEventListener("submit", handleSubmit);
